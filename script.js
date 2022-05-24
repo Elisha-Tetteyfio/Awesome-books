43659332227bdb0058e370efb4b91dc2c21a4db7
Robertson Akpan <robertsonakpan@gmail.com>
2022-05-24 21:11:58 +0100

    Add LocalStorage functionality

--- script.js
+++ script.js
@@ -1,54 +1,123 @@
-var bookList = [
-  {
-    title: 'Lorem ipsum',
-    author: 'Author 1',
-    id: 1
-  },
-  {
-    title: 'Second Book',
-    author: 'Author 2',
-    id: 2
-  }
-]
+let bookList = getBooks();
 
-function add (book){
-  book.id = bookList.length + 1;
-  bookList.push(book);
-}
-
+// Book Constructor function
 function Book(title, author){
   this.title = title;
   this.author = author;
   this.id = bookList.length + 1;
 }
 
-function remove(myBook){
-  bookList = bookList.filter(book => book.id !== myBook.id);
+// Book Card Template
+function bookCard({title, author, id}){
+  const cardHolder = document.createElement('div');
+  cardHolder.classList.add('book-card');
+  cardHolder.id = id; //Each book card holder assigned book id
+
+  const titleEl = document.createElement('div');
+  titleEl.textContent = title;
+
+  const authorEl = document.createElement('div');
+  authorEl.textContent = author;
+
+  const removeBtn = document.createElement('button');
+  removeBtn.type = 'button';
+  removeBtn.textContent = 'remove';
+  removeBtn.classList.add('remove-btn');
+
+  const hr = document.createElement('hr');
+
+  cardHolder.append(titleEl, authorEl, removeBtn, hr);
+  return cardHolder; 
+};
+
+// Display books in Booklist on page load 
+function bookDisplay(){
+  const bookContainer = document.querySelector('.booksContainer');   
+  bookList.forEach(book => bookContainer.appendChild(bookCard(book)));
+};
+
+// Add Book to Array & UI
+function addBookUI(book){
+  const bookContainer = document.querySelector('.booksContainer');
+  bookContainer.appendChild(bookCard(book))
+  bookList = [...bookList, book]; //Add Book to bookList. Alternative to bookList.push(book)  
+};
+
+// Clear form fields
+function clearFormFields(){
+  document.querySelector('#book-name').value = '';   
+  document.querySelector('#book-author').value = ''; 
 }
 
-const bookAuthor = document.querySelector('.addAuthor');
-const bookTitle = document.querySelector('.addTitle');
-
-const addButton = document.querySelector(".addButton");
-addButton.addEventListener('click', ()=> {
-  if(bookAuthor.value != '' && bookTitle.value != ''){
-    bookList.push(new Book(bookAuthor.value, bookTitle.value));
-  }
+// Remove book
+function removeBook(id) {   
+  bookList = bookList.filter(item => (item.id).toString() !== id);
   console.log(bookList);
-})
+}
 
-// var bookSegment = document.createElement('div');
-// const header = document.querySelector('.header');
-// header.after(bookSegment);
-// var bookSegment1 = '';
+// Local Storage
+function getBooks(){
+  let books;
+  if(localStorage.getItem('bookList') === null){
+      books = [];
+  }else {
+      books = JSON.parse(localStorage.getItem('bookList'));
+  }
+  return books;
+}
 
-// bookList.forEach(book => {
-//   bookSegment1 += `
-//   <div>
-//     <p> ${this.title} </p>
-//     <p> ${this.author} </p>
-//   </div>
-//   `
-// });
+function addBookToLS(book){
+  const books = getBooks();
+  books.push(book);
+  localStorage.setItem('bookList', JSON.stringify(books));
+}
 
-// bookSegment.insertAdjacentHTML('afterend',bookSegment1);
+function removeBookLS(id){
+  const books = getBooks();
+  books.forEach((book, index) => {
+      if((book.id).toString() === id){
+          books.splice(index,1);
+      };
+  }); 
+  localStorage.setItem('bookList', JSON.stringify(books));
+};
+
+
+
+// All Events
+    // Display books on page load
+    window.addEventListener('DOMContentLoaded', bookDisplay);
+
+    // Add Book Event
+    const formEl = document.querySelector('#book-form');
+    formEl.addEventListener('click', (e) => {
+        e.preventDefault();
+        let title = document.querySelector('#book-name').value;
+        let author = document.querySelector('#book-author').value;
+
+        if(title !== '' && author !== ''){
+          if(e.target.classList.contains('addButton')){
+            let newBook = new Book(title, author);
+            addBookUI(newBook);
+            addBookToLS(newBook);  //Add book to booklist in localStorage         
+            clearFormFields();                     
+            formEl.submit();
+          }; 
+        }else{
+          return ;
+        }          
+    });
+
+    // Remove Book from UI, localStorage and arrayList
+    const bookContainer = document.querySelector('.booksContainer');
+    bookContainer.addEventListener('click', (e) => {      
+
+        if(e.target.classList.contains('remove-btn')){
+            let bookId = e.target.parentElement.id //Card holder ID traversed here
+            console.log(bookId);
+            removeBook(bookId);
+            removeBookLS(bookId);
+            e.target.parentElement.remove();           
+        };
+    });
+
